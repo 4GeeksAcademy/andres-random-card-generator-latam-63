da4798ac5eceb2e51b91c2988c4ea10559d9e7e2
AEGiner <andresesquivel2011@gmail.com>
2025-10-28 00:39:03 +0000

    Created HTML, CSS styles and Script for a random card generator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,55 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = function () {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suits = ["&#9824;", "&#9827;", "&#9829;", "&#9830;",]
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+  function randomCard(array) {
+    return Math.floor(Math.random() * array.length)
+  }
+  let newCardSuit = suits[randomCard(suits)]
+  let newCard = document.createElement('div')
+  newCard.classList.add('card')
+  newCard.innerHTML = `<div class="symbol-start">${newCardSuit}</div>
+                       <div class="number">${numbers[randomCard(numbers)]}</div>
+                       <div class="symbol-end">${newCardSuit}</div>`
+  let parent = document.getElementById('card-container');
+  parent.appendChild(newCard);
+
+  if (newCardSuit == '&#9824;' || newCardSuit == '&#9827;') {
+    newCard.classList.add('black');
+  }
 };
+document.getElementById('button').addEventListener('click', function () {
+  let suits = ["&#9824;", "&#9827;", "&#9829;", "&#9830;",]
+  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+  function randomCard(array) {
+    return Math.floor(Math.random() * array.length)
+  }
+  let newCardSuit = suits[randomCard(suits)]
+  let newCard = document.createElement('div')
+  newCard.classList.add('card')
+  newCard.innerHTML = `<div class="symbol-start">${newCardSuit}</div>
+                       <div class="number">${numbers[randomCard(numbers)]}</div>
+                       <div class="symbol-end">${newCardSuit}</div>`
+  let parent = document.getElementById('card-container');
+  parent.appendChild(newCard);
+
+  if (newCardSuit == '&#9824;' || newCardSuit == '&#9827;') {
+    newCard.classList.add('black');
+  }
+})
+
+document.getElementById('change-size').addEventListener('click', function(){
+
+  let cardSize = parseInt(document.getElementById('card-scale').value) / 350
+  let cards = document.getElementsByClassName('card')
+  for(let i of cards){
+    i.style.scale = cardSize
+  }
+
+  console.log(cards)
+})
