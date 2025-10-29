3acf2c4021de264cfc8748caa17c6385ddfa0201
AEGiner <andresesquivel2011@gmail.com>
2025-10-29 22:29:12 +0000

    Added auto generated card with timer, and fixed unnecessary code

--- src/app.js
+++ src/app.js
@@ -1,12 +1,10 @@
 import "bootstrap";
 import "./style.css";
 
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function () {
-  //write your code here
+function createNewcard() {
   let suits = ["&#9824;", "&#9827;", "&#9829;", "&#9830;",]
   let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
@@ -25,32 +23,14 @@ window.onload = function () {
   if (newCardSuit == '&#9824;' || newCardSuit == '&#9827;') {
     newCard.classList.add('black');
   }
-};
-
-document.getElementById('button').addEventListener('click', function () {
-
-  let suits = ["&#9824;", "&#9827;", "&#9829;", "&#9830;",]
-  let numbers = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
-
-  function randomCard(array) {
-    return Math.floor(Math.random() * array.length)
-  }
-  let newCardSuit = suits[randomCard(suits)]
-  let newCard = document.createElement('div')
-  newCard.classList.add('card')
-  newCard.innerHTML = `<div class="symbol-start">${newCardSuit}</div>
-                       <div class="number">${numbers[randomCard(numbers)]}</div>
-                       <div class="symbol-end">${newCardSuit}</div>`
-  let parent = document.getElementById('card-container');
-  parent.appendChild(newCard);
 
   let cardSize = parseInt(document.getElementById('card-scale').value) / 350
   newCard.style.scale = cardSize
+};
 
-  if (newCardSuit == '&#9824;' || newCardSuit == '&#9827;') {
-    newCard.classList.add('black');
-  }
-})
+window.onload = createNewcard;
+
+document.getElementById('button').addEventListener('click', createNewcard);
 
 document.getElementById('change-size').addEventListener('click', function () {
 
@@ -59,4 +39,12 @@ document.getElementById('change-size').addEventListener('click', function () {
   for (let i of cards) {
     i.style.scale = cardSize
   }
-})
+});
+
+let cardInterval = setInterval(createNewcard, 1000)
+document.getElementById('stopTimer').addEventListener('click', function () {
+  clearInterval(cardInterval);
+});
+
+
+
